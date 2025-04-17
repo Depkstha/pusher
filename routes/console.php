2b9e5925bf0d0a794a76eb8a3bb7646a5ac1dbe8
dharma <dharmaraj@bibhutisolutions.com>
2025-04-17 23:17:06 +0545

    changes

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
 
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+    Log::info(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 Schedule::call(function () {
